f6fec21c358989cbf23e2653dd1282040958844e
Bullet Rout <routbullet@gmail.com>
2020-08-07 12:49:48 +0530

    updated
    banner gradient

--- src/components/AppBanner.js
+++ src/components/AppBanner.js
@@ -7,15 +7,25 @@ const Banner = styled.img`
   width: 100%;
   height: 100vh;
   object-fit: cover;
-
   background-position: center center;
+`;
+
+const BannerBottomGradient = styled.div`
+  height: 7.8rem;
+  position: absolute;
+  width: 100%;
+
+  margin-top: -6.6rem;
+  z-index: 2;
+
+  background-image: linear-gradient(180deg, transparent, #0d0c1d, #0d0c1d);
 `;
 
 export default function AppBanner() {
   const [banner, setBanner] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(Requests.fetchUpcoming);
+      const request = await axios.get(Requests.fetchTrending);
       setBanner(
         request.data.results[
           Math.round(Math.random() * request.data.results.length - 1)
@@ -31,6 +41,7 @@ export default function AppBanner() {
   return (
     <div>
       <Banner src={`${TMDB_ImageUrl}${banner?.backdrop_path}`} alt="" />
+      <BannerBottomGradient></BannerBottomGradient>
     </div>
   );
 }
